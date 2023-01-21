41e77ef348512a13906d41a1e7f1adeaa2432567
Loknath2001 <uccloknath2001@gmail.com>
2023-01-21 22:07:33 +0530

    first commit

---
+++ One.java
@@ -0,0 +1,15 @@
+public class One{
+ public static void main(String args[]){
+     byte numberOfSeats = 7;
+     byte numberOfDoors = 4;
+     byte numberOfOwners = 5;
+
+     short power = 362;
+     short horsePower = 496;
+
+     int price = 299999;
+     
+     long registrationNumber = 3454576575;
+  
+ }
+}
